64c4088bba7e89caf1c7ec0bba310c3e045b4ca0
aoc55 <aoc55.soft@gmail.com>
2024-09-05 17:34:57 +0900

    chore: Local/DEV 환경에서 초기 데이터 주입처리

--- src/main/java/com/aoc55/demoboard/config/InitSampleDataTool.java
+++ src/main/java/com/aoc55/demoboard/config/InitSampleDataTool.java
@@ -5,10 +5,7 @@ import com.aoc55.demoboard.domain.board.BoardRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 /**
@@ -17,10 +14,10 @@ import org.springframework.stereotype.Component;
  */
 
 @Slf4j
-@Profile("local")
+@Profile({"local", "dev"})
 @Component
 @RequiredArgsConstructor
-public class LocalInitData {
+public class InitSampleDataTool {
 
     private final BoardRepository boardRepository;
 
@@ -33,7 +30,7 @@ public class LocalInitData {
         boardRepository.save(Board.builder().title("안녕하세요").content("데모 텍스트1입니다").writer("김익명").password("1234").build());
         boardRepository.save(Board.builder().title("반갑습니다").content("데모 텍스트2입니다").writer("이익명").password("4567").build());
         boardRepository.save(Board.builder().title("어서오세요").content("데모 텍스트3입니다").writer("박익명").password("1234").build());
-        log.info("로컬용 초기 데이터 주입완료");
+        log.info("로컬/DEV 용 초기 데이터 주입완료");
     }
 
 }
